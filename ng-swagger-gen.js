3c2ac040068b262fa6b583576d9f31eea104901e
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2017-04-04 16:26:48 -0300

    Several changes for the 0.2.0 version
    Includes:
    - License change (MIT)
    - Support for a configuration file
    - Support for specifying which tags to generate
    - Support for removing stale files
    - Support for specifying whether to generate model / service indexes and api module
    - Bug fixes

--- ng-swagger-gen.js
+++ ng-swagger-gen.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const url = require('url');
 const http = require('http');
+const https = require('https');
 const path = require('path');
 const Mustache = require('mustache');
 
@@ -16,9 +17,11 @@ function ngSwaggerGen(options) {
   }
 
   var u = url.parse(options.swagger);
-  if (u.protocol === 'http:' || u.protocol === 'https:') {
-    // The swagger definition is an HTTP URL - fetch it
-    http.get(options.swagger, (res) => {
+  var isHttp = u.protocol === 'http:';
+  var isHttps = u.protocol === 'https:';
+  if (isHttp || isHttps) {
+    // The swagger definition is an HTTP(S) URL - fetch it
+    (isHttp ? http : https).get(options.swagger, (res) => {
       const statusCode = res.statusCode;
       const contentType = res.headers['content-type'];
 
@@ -60,7 +63,11 @@ function ngSwaggerGen(options) {
  * Proceedes with the generation given the swagger descriptor content
  */
 function doGenerate(swaggerContent, options) {
-  var templates = options.templates || 'templates';
+  if (!options.templates) {
+    options.templates = path.join(__dirname, 'templates');
+  }
+
+  var templates = options.templates;
   var output = options.output || 'src/app/api';
 
   var swagger = JSON.parse(swaggerContent);
@@ -77,6 +84,13 @@ function doGenerate(swaggerContent, options) {
   var models = processModels(swagger);
   var services = processServices(swagger, models);
 
+  // Apply the tag filter. If includeTags is null, uses all services, but still removes unused models
+  var includeTags = options.includeTags;
+  if (typeof includeTags == 'string') {
+    includeTags = includeTags.split(",");
+  }
+  applyTagFilter(models, services, includeTags, options.ignoreUnusedModels !== false);
+
   // Read the templates
   var templates = {}
   var files = fs.readdirSync(options.templates);
@@ -89,13 +103,12 @@ function doGenerate(swaggerContent, options) {
   });
 
   // Prepare the output folder
-  const modelsOutput = output + '/models';
-  const servicesOutput = output + '/services';
+  const modelsOutput = path.join(output, '/models');
+  const servicesOutput = path.join(output, '/services');
+  mkdirs(modelsOutput);
+  mkdirs(servicesOutput);
 
-  //if (fs.existsSync(output)) rmdirRecursive(output);
-  if (!fs.existsSync(output)) fs.mkdirSync(output);
-  if (!fs.existsSync(modelsOutput)) fs.mkdirSync(modelsOutput);
-  if (!fs.existsSync(servicesOutput)) fs.mkdirSync(servicesOutput);
+  var removeStaleFiles = options.removeStaleFiles === true;
 
   // Utility function to render a template and write it to a file
   var generate = function (template, model, file) {
@@ -114,10 +127,30 @@ function doGenerate(swaggerContent, options) {
   if (modelsArray.length > 0) {
     modelsArray[modelsArray.length - 1].last = true;
   }
+  if (removeStaleFiles) {
+    var modelFiles = fs.readdirSync(modelsOutput);
+    modelFiles.forEach((file, index) => {
+      var ok = false;
+      var basename = path.basename(file);
+      for (var modelName in models) {
+        var model = models[modelName];
+        if (basename == model.modelFile + '.ts') {
+          ok = true;
+          break;
+        }
+      }
+      if (!ok) {
+        rmIfExists(path.join(modelsOutput, file));
+      }
+    });
+  }
 
   // Write the model index
-  {
-    generate(templates.models, { "models": modelsArray }, output + "/models.ts");
+  var modelIndexFile = output + "/models.ts";
+  if (options.modelIndex !== false) {
+    generate(templates.models, { "models": modelsArray }, modelIndexFile);
+  } else if (removeStaleFiles) {
+    rmIfExists(modelIndexFile);
   }
 
   // Write the services
@@ -130,15 +163,38 @@ function doGenerate(swaggerContent, options) {
   if (servicesArray.length > 0) {
     servicesArray[servicesArray.length - 1].last = true;
   }
+  if (removeStaleFiles) {
+    var serviceFiles = fs.readdirSync(servicesOutput);
+    serviceFiles.forEach((file, index) => {
+      var ok = false;
+      var basename = path.basename(file);
+      for (var serviceName in services) {
+        var service = services[serviceName];
+        if (basename == service.serviceFile + '.ts') {
+          ok = true;
+          break;
+        }
+      }
+      if (!ok) {
+        rmIfExists(path.join(servicesOutput, file));
+      }
+    });
+  }
 
   // Write the service index
-  {
-    generate(templates.services, { "services": servicesArray }, output + "/services.ts");
+  var serviceIndexFile = output + "/services.ts";
+  if (options.serviceIndex !== false) {
+    generate(templates.services, { "services": servicesArray }, serviceIndexFile);
+  } else if (removeStaleFiles) {
+    rmIfExists(serviceIndexFile);
   }
 
   // Write the api module
-  {
-    generate(templates.apiModule, { "services": servicesArray }, output + "/api.module.ts");
+  var apiModuleFile = output + "/api.module.ts";
+  if (options.apiModule !== false) {
+    generate(templates.apiModule, { "services": servicesArray }, apiModuleFile);
+  } else if (removeStaleFiles) {
+    rmIfExists(apiModuleFile);
   }
 
   // Write the ApiConfiguration
@@ -153,20 +209,80 @@ function doGenerate(swaggerContent, options) {
 }
 
 /**
- * Recursively removes a directory with all its subfolders / files
+ * Applies a filter over the given services, keeping only the specific tags.
+ * Also optionally removes any unused models, even if includeTags is null (meaning all).
  */
-function rmdirRecursive(dir) {
-  if (fs.existsSync(dir)) {
-    var files = fs.readdirSync(dir);
-    files.forEach(function (file, index) {
-      var curPath = path.join(dir, file);
-      if (fs.lstatSync(curPath).isDirectory()) {
-        rmdirRecursive(curPath);
-      } else {
-        fs.unlinkSync(curPath);
+function applyTagFilter(models, services, includeTags, ignoreUnusedModels) {
+  var usedModels = new Set();
+  for (var serviceName in services) {
+    var include = !includeTags || includeTags.indexOf(serviceName) >= 0;
+    if (!include) {
+      // This service is skipped - remove it
+      console.log("Ignoring service " + serviceName + " because it was not included");
+      delete services[serviceName];
+    } else if (ignoreUnusedModels) {
+      // Collect the models used by this service
+      var service = services[serviceName];
+      service.serviceDependencies.forEach((dep, index) => usedModels.add(dep));
+    }
+  }
+
+  if (ignoreUnusedModels) {
+    // Collect the model dependencies of models, so unused can be removed
+    var allDependencies = new Set();
+    usedModels.forEach(dep => collectDependencies(allDependencies, dep, models));
+
+    // Remove all models that are unused
+    for (var modelName in models) {
+      if (!allDependencies.has(modelName)) {
+        // This model is not used - remove it
+        console.log("Ignoring model " + modelName + " because it was not used by any service");
+        delete models[modelName];
       }
-    });
-    fs.rmdirSync(dir);
+    }
+  }
+}
+
+/**
+ * Collects on the given dependencies set all dependencies of the given model name
+ */
+function collectDependencies(dependencies, model, models) {
+  if (!model || dependencies.has(model.modelName)) {
+    return;
+  }
+  dependencies.add(model.modelName);
+  if (model.modelDependencies) {
+    model.modelDependencies.forEach((dep, index) =>
+      collectDependencies(dependencies, dep, models));
+  }
+}
+
+/**
+ * Creates all sub-directories for a nested path
+ * Thanks to https://github.com/grj1046/node-mkdirs/blob/master/index.js
+ */
+function mkdirs(folderPath, mode) {
+    var folders = [];
+    var tmpPath = path.normalize(folderPath);
+    var exists = fs.existsSync(tmpPath);
+    while (!exists) {
+        folders.push(tmpPath);
+        tmpPath = path.join(tmpPath, '..');
+        exists = fs.existsSync(tmpPath);
+    }
+
+    for (var i = folders.length - 1; i >= 0; i--) {
+        fs.mkdirSync(folders[i], mode);
+    }
+}
+
+/**
+ * Removes the given file if it exists (logging the action)
+ */
+function rmIfExists(file) {
+  if (fs.existsSync(file)) {
+    console.log("Removing stale file " + file);
+    fs.unlinkSync(file);
   }
 }
 
@@ -319,7 +435,8 @@ function processModels(swagger) {
       "modelIsEnum": enumValues != null,
       "properties": properties == null ? null :
         processProperties(swagger, properties, requiredProperties),
-      "modelEnumValues": enumValues
+      "modelEnumValues": enumValues,
+      "modelSubclasses": []
     };
 
     if (descriptor.properties != null) {
@@ -341,7 +458,26 @@ function processModels(swagger) {
     models[name] = descriptor;
   }
 
-  // Now that we know all modules, resolve the dependencies
+  // Now that we know all models, process the hierarchies
+  for (var name in models) {
+    var model = models[name];
+    if (!model.modelIsObject) {
+      // Only objects can have hierarchies
+      continue;
+    }
+
+    // Process the hierarchy
+    var parentName = model.modelParent;
+    if (parentName) {
+      // Make the parent be the actual model, not the name
+      model.modelParent = models[parentName];
+
+      // Append this model on the parent's subclasses
+      model.modelParent.modelSubclasses.push(model);
+    }
+  }
+
+  // Now that the model hierarchy is ok, resolve the dependencies
   for (var name in models) {
     var model = models[name];
     if (!model.modelIsObject) {
@@ -349,13 +485,31 @@ function processModels(swagger) {
       continue;
     }
     var dependencies = new DependenciesResolver(models, model.modelName);
-    dependencies.add(model.modelParent);
 
+    // The parent is a dependency
+    if (model.modelParent) {
+      dependencies.add(model.modelParent.modelName);
+    }
+
+    // The subclasses are dependencies
+    for (var i = 0; i < model.modelSubclasses.length; i++) {
+      var child = model.modelSubclasses[i];
+      dependencies.add(child.modelName);
+    }
+
+    // Each property may add a dependency
     for (var i = 0; i < model.modelProperties.length; i++) {
       var property = model.modelProperties[i];
-      dependencies.add(property.propertyType);
+      var type = property.propertyType;
+      if (type.allTypes) {
+        // This is an inline object. Append all types
+        type.allTypes.forEach((t, i) => dependencies.add(t));
+      } else {
+        dependencies.add(type);
+      }
     }
     model.modelDependencies = dependencies.get();
+
   }
 
   return models;
@@ -377,9 +531,20 @@ function propertyType(property) {
   if (property == null) {
     return "void";
   } else if (property.$ref != null) {
+    // Type is a reference
     return simpleRef(property.$ref);
+  } else if (property["x-type"]) {
+    // Type is read from the x-type vendor extension
+    var type = (property["x-type"] || "").toString().replace("List<", "Array<");
+    var pos = type.indexOf("Array<");
+    if (pos >= 0) {
+      type = type.substr("Array<".length, type.length - 1) + "[]";
+    }
+    return type.length == 0 ? 'void' : type;
   }
   switch (property.type) {
+    case "string":
+      return "string";
     case "array":
       return propertyType(property.items) + "[]";
     case "integer":
@@ -387,8 +552,42 @@ function propertyType(property) {
       return "number";
     case "boolean":
       return "Boolean";
+    case "object":
+      var def = "{";
+      var first = true;
+      var allTypes = [];
+      if (property.properties) {
+        for (var name in property.properties) {
+          var prop = property.properties[name];
+          if (first) {
+            first = false;
+          } else {
+            def += ", ";
+          }
+          var type = propertyType(prop);
+          if (allTypes.indexOf(type) < 0) {
+            allTypes.push(type);
+          }
+          def += name + ": " + type;
+        }
+      }
+      if (property.additionalProperties) {
+        if (!first) {
+          def += ", ";
+        }
+        var type = propertyType(property.additionalProperties);
+        if (allTypes.indexOf(type) < 0) {
+          allTypes.push(type);
+        }
+        def += "[key: string]: " + type;
+      }
+      def += "}";
+      return {
+        allTypes: allTypes,
+        toString: () => def
+      };
     default:
-      return "string";
+      return "any";
   }
 }
 
@@ -429,17 +628,32 @@ function resolveRef(swagger, ref) {
 }
 
 /*
- * Returns the operation result type
+ * Process an operation's possible responses. Returns an object keyed
+ * by each HTTP code, whose values are objects with code and type properties,
+ * plus a property resultType, which is the type to the HTTP 2xx code.
  */
-function processResultType(def, path, models) {
+function processResponses(def, path, models) {
   var responses = def.responses || {};
+  var operationResponses = {};
   for (var code in responses) {
+    var response = responses[code];
+    if (!response.schema) {
+      continue;
+    }
+    var type = propertyType(response.schema);
     if (/2\d\d/.test(code)) {
       // Successful response
-      return propertyType(responses[code].schema);
+      operationResponses.resultType = type;
     }
+    operationResponses[code] = {
+      "code": code,
+      "type": type
+    };
   }
-  return "void";
+  if (!operationResponses.resultType) {
+    operationResponses.resultType = 'void';
+  }
+  return operationResponses;
 }
 
 /**
@@ -497,6 +711,12 @@ function processServices(swagger, models) {
         if (param.$ref) {
           param = resolveRef(swagger, param.$ref);
         }
+        var paramType;
+        if (param.schema) {
+          paramType = propertyType(param.schema);
+        } else {
+          paramType = propertyType(param);
+        }
         var paramDescriptor = {
           "paramName": param.name,
           "paramIn": param.in,
@@ -508,7 +728,7 @@ function processServices(swagger, models) {
           "paramIsArray": param.type === 'array',
           "paramDescription": param.description,
           "paramComments": toComments(param.description, 1),
-          "paramType": propertyType(param),
+          "paramType": paramType,
           "paramCollectionFormat": param.collectionFormat
         };
         operationParameters.push(paramDescriptor);
@@ -524,7 +744,8 @@ function processServices(swagger, models) {
       }
       var paramsClass = operationParameters.length == 0
         ? null : id.charAt(0).toUpperCase() + id.substr(1) + "Params";
-      var resultType = processResultType(def, path, models);
+      var operationResponses = processResponses(def, path, models);
+      var resultType = operationResponses.resultType;
       var docString = def.description || "";
       for (var i = 0; i < operationParameters.length; i++) {
         var param = operationParameters[i];
@@ -538,7 +759,8 @@ function processServices(swagger, models) {
         "operationPathExpression": toPathExpression(url),
         "operationComments": toComments(docString, 1),
         "operationResultType": resultType,
-        "operationParameters": operationParameters
+        "operationParameters": operationParameters,
+        "operationResponses": operationResponses
       }
       operation.operationIsVoid = resultType === 'void';
       operation.operationIsString = resultType === 'string';
@@ -559,7 +781,10 @@ function processServices(swagger, models) {
     var dependencies = new DependenciesResolver(models);
     for (var i = 0; i < descriptor.serviceOperations.length; i++) {
       var op = descriptor.serviceOperations[i];
-      dependencies.add(op.operationResultType);
+      for (var code in op.operationResponses) {
+        var response = op.operationResponses[code]
+        dependencies.add(response.type);
+      }
       for (var j = 0; j < op.operationParameters.length; j++) {
         var param = op.operationParameters[j];
         dependencies.add(param.paramType);
